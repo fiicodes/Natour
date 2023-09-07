8bd5546d031fabf8ba6dc035a31ebc87d0d930cf
fithaashraf17 <fithaashraf172@gmail.com>
2023-09-07 13:43:37 +0530

    implemented filtering,sorting and pagination to api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,7 +12,40 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //build query
+    //1)Filtering
+    // console.log(req.query);
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    //2)advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2 sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+    //3 limitimg fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //4 pagination
+    const page = req.query.page + 1 || 1;
+    const limit = req.query.limit + 1 || 100;
+    const skip = page - 1 + limit;
+    query = query.skip(skip).limit(limit);
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
